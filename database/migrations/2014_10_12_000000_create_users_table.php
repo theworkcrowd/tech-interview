48f75ab33d4819276a335dcfa6d59dc4683fc356
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2022-03-15 18:44:58 +0200

    ...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+      Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->string('first_name');
+        $table->string('last_name');
+        $table->string('avatar')->unique();
+        $table->timestamps();
+      });
     }
 
     /**
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+      Schema::dropIfExists('users');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="row justify-content-center">
+    <ul>
+      @foreach ($users as $user)
+        <li>
+          <img src="{{$user['avatar']}}" style="width:60px" alt="" />
+          {{$user['first_name']}}
+          {{$user['last_name']}}
+        </li>
+      @endforeach
+    </ul>
+  </div>
 </div>
 @endsection
