f92cc07f5c17b2762c796a3b1f574d5344a80b64
Ronald <kyoberonald@gmail.com>
2022-10-10 18:03:38 +0300

    Made user pics and names more visible, changed from list to card group
    Style the users list with bootstrap in the resources/views/home.blade.php blade file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <ul>
-      @foreach ($users as $user)
-        <li>
-          <img src="{{$user['avatar']}}" style="width:60px" alt="" />
-          {{$user['first_name']}}
-          {{$user['last_name']}}
-        </li>
-      @endforeach
-    </ul>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="card-group">
+{{--      <ul>--}}
+        @foreach ($users as $user)
+          <div class="col-md-3 mt-5">
+            <div class="card">
+{{--              <li>--}}
+                <img src="{{$user['avatar']}}" class="img-fluid" alt=""/>
+                <b>{{$user['first_name']}} {{$user['last_name']}}</b>
+{{--              </li>--}}
+            </div>
+          </div>
+        @endforeach
+{{--      </ul>--}}
+      </div>
+    </div>
   </div>
-</div>
 @endsection
